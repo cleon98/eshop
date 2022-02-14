1026e0764f3217982a94037a5c4de0c92afbab71
cleon98 <c.l3on98@gmail.com>
2022-02-14 01:15:12 -0500

    functional shopping cart and whatsapp

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from "../styles/CartProduct.module.css"
+import { CartContext } from '../CartContext'
 
 export const CartProduct = (props) => {
-    const {id, description, price, size, img} = props
+    const { description, price, size, category, img, id} = props
+
+    const  [cart, setCart] = useContext(CartContext);
+
+    const removeProduct = (id) => {
+      cart.forEach((item, index) => {
+        if (item.id === id) {
+          cart.splice(index, 1);
+        }
+      });
+      setCart([...cart]);
+    };
+  
   return( 
     
   <div className={styles.cardOfProduct}>
@@ -10,10 +23,11 @@ export const CartProduct = (props) => {
       <img className={styles.infoProductImg} alt="pictures of the products" src={img}/>
       </div>
       <div className={styles.cartInformation}>
-      <h2 className={styles.infoProduct}>{props.category}</h2>
+      <h2 className={styles.infoProduct}>{category}</h2>
       <p className={styles.infoProduct}>{description}</p>
       <h3 className={styles.infoProduct}>$ {price}</h3>
       <h3 className={styles.infoProduct}> Tamaño: {size}</h3>
+      <button onClick={()=>removeProduct(cart.id)} className={styles.buttonRemove}>Eliminar producto</button>
       </div>
   </div>
   )
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -6,12 +6,28 @@ import CartProduct from '../components/CartProduct';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from "../styles/ShoppingCart.module.css"
+import GoToTop from '../components/onTop';
 
 
 const ShoppingCart = (props) => {
 
   const  [cart, setCart] = useContext(CartContext);
   const totalPrice = cart.reduce((accumulate, current) => accumulate + current.price, 0) 
+
+  const sendOrder = () => {
+    let string = ["Hola Evren. Me gustaria adquirir los siguientes products: "];
+    cart.forEach((element) => {
+      const { description, size, price } = element;
+      string.push(`Producto: ${description} Talla: ${size} Precio: ${price}`);
+    });
+    let key = string.join(" - ");
+    let url = key.replace(/\s+/g, "%20");
+    window.open(`https://wa.me/573154580599?text=${url}`);
+
+    cart.splice(0, cart.length);
+    setCart([...cart]);
+  };
+
 
  console.log(cart)
 
@@ -41,9 +57,10 @@ const ShoppingCart = (props) => {
           </section>
       <div className={styles.totalPrice}>Precio total: $ {totalPrice} </div>
         <div className={styles.buttonsCart}>
-      <button className={styles.buttonBuying}>Realizar tu pedido</button>
+      <button onClick={sendOrder} className={styles.buttonBuying}>Realizar tu pedido</button>
       <Link className={styles.backToShop} to="/productos"><button className={styles.buttonBuying}>Volver a la tienda</button></Link>
       </div>
+      <GoToTop/>
       <Footer/>
     </div>
   )
